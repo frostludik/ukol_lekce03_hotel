7edee4269121153dc1befa5c560518347958aad5
Ludek Mraz <ludek.mraz@centrum.cz>
2024-02-13 16:41:06 +0100

    new method getAverageGuests

--- src/cz/engeto/hotel/BookingManager.java
+++ src/cz/engeto/hotel/BookingManager.java
@@ -6,18 +6,21 @@ import java.util.List;
 public class BookingManager {
     private List<Booking> bookingList;
     private Booking booking;
+    private Booking isVacation;
 
 
-    public BookingManager() {
+    public BookingManager(List<Booking> bookingList, Booking booking, Booking isVacation) {
         this.bookingList = new ArrayList<>();
+        this.booking = booking;
+        this.isVacation = isVacation;
 
     }
-
 
 
     public void addBooking(Booking newBooking) {
         this.bookingList.add(newBooking);
     }
+
 
     public Booking getBooking(int index) {
         if (index >= 0 && index < bookingList.size()) {
@@ -36,4 +39,21 @@ public class BookingManager {
     }
 
 
+    public int getNumberOfWorkingBookings() {
+        int countOfWorkingBookings = 0;
+        for (Booking booking : bookingList) {
+            if (booking.isVacation() == null) {
+                countOfWorkingBookings++;
+            }
+        }
+        return countOfWorkingBookings;
+    }
+
+    public double getAverageGuests() {
+        int totalNumberOfGuest = 0;
+        for (Booking booking : bookingList) {
+            totalNumberOfGuest += booking.getNumberOfGuests();
+        }
+        return (double) totalNumberOfGuest / bookingList.size();
+    }
 }
