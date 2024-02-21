74413b38710679ebb88eb49b11f5df95c3bc2ef8
Ludek Mraz <ludek.mraz@centrum.cz>
2024-02-21 13:32:30 +0100

    Extracted methods in main

--- src/Main.java
+++ src/Main.java
@@ -9,74 +9,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
 
-    List<Guest> guestsList = new ArrayList<>();
-    List<Room> roomsList = new ArrayList<>();
-    BookingManager bookingList = new BookingManager();
+        List<Guest> guestsList = new ArrayList<>();
+        List<Room> roomsList = new ArrayList<>();
+        BookingManager bookingList = new BookingManager();
 
 //creating guests
-    private void createGuests () {
-        guestsList.add(new Guest("Karel", "Dvořák", LocalDate.of(1990, 5, 15)));
-        guestsList.add(new Guest("Karel", "Dvořák", LocalDate.of(1979, 1, 3)));
-        guestsList.add(new Guest("Karolína", "Tmavá", LocalDate.of(1981, 6, 3)));
-    }
-
-//creating rooms
-    private void createRooms () {
-        roomsList.add(new Room(1, 1, true, true, BigDecimal.valueOf(1000)));
-        roomsList.add(new Room(2, 1, true, true, BigDecimal.valueOf(1000)));
-        roomsList.add(new Room(3, 3, false, true, BigDecimal.valueOf(2400)));
-    }
-
+        createGuests(guestsList);
+//creating room
+        createRooms(roomsList);
 //adding bookings
-    private void createBookings () {
-        bookingList.addBooking(new Booking(roomsList.get(2), guestsList.get(0), LocalDate.of(2023, 6, 1),
-                LocalDate.of(2023, 6, 7), false));
-
-        bookingList.addBooking(new Booking(roomsList.get(1), guestsList.get(1), LocalDate.of(2023, 7, 18),
-                LocalDate.of(2023, 7, 21), true));
-
-        //adding multiple bookings with same parameters, but consequent dates
-        LocalDate startDate = LocalDate.of(2023, 8, 1);
-        for (int i = 0; i < 10; i++) {
-            LocalDate endDate = startDate.plusDays(1);
-            bookingList.addBooking(new Booking(roomsList.get(1), guestsList.get(2), startDate, endDate, true));
-            startDate = endDate.plusDays(1);
-        }
-        bookingList.addBooking(new Booking(roomsList.get(2), guestsList.get(2), LocalDate.of(2023, 8, 1),
-                LocalDate.of(2023, 8, 31), true));
-    }
-
+        addBookings(bookingList, roomsList, guestsList);
 //print all booking with details
-    private void printAllBookings() {
-        System.out.println("\n-------all bookings----------------------------------------------------------------------");
-        for (int i = 0; i < bookingList.getNumberOfBookings(); i++) {
-            String seaViewAvailability = bookingList.getBooking(i).isHasSeaView() ? "room with seaview" : "room without seaview";
-            System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
-                    + ": " + bookingList.getBooking(i).getGuest() + ", [total guests: " + bookingList.getBooking(i).getNumberOfGuests() + ", "
-                    + seaViewAvailability + "] for " + bookingList.getBooking(i).getPricePerNight() + " CZK");
-        }
-    }
-
+        printAllBookingsWithDetails(bookingList);
 //print first 8 vacation bookings
-    private void printFirstEightVacationBookings() {
-        System.out.println("\n-------First eight vacation bookings------------------------------------------------------");
-        int firstEight = 0;
+        printFirstEightVacationBookings(bookingList);
+//print guest statistics
+        printGuestStatistics(bookingList);
+//print total price per booking
+        printTotalPricePerBooking(bookingList);
+    }
+
+    private static void printTotalPricePerBooking(BookingManager bookingList) {
+        System.out.println("\n-------Total price per booking-----------------------------------------------------------");
         for (int i = 0; i < bookingList.getNumberOfBookings(); i++) {
-            if (bookingList.getBooking(i).isVacation()) {
-                String vacationOrBusiness = bookingList.getBooking(i).isVacation() ? "vacation" : "business";
-                System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
-                        + ": " + bookingList.getBooking(i).getGuest() + ", [total guests: " + bookingList.getBooking(i).getNumberOfGuests() + ", "
-                        + "room with seaview" + "] for " + bookingList.getBooking(i).getPricePerNight() +
-                        " CZK, type of stay: " + vacationOrBusiness);
-                firstEight++;
-                if (firstEight == 8) break;
-            }
+            System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
+                    + ": " + bookingList.getBooking(i).getGuest() + ", " + bookingList.getBooking(i).getBookingLength()
+                    + " nights, Total price of booking: " + bookingList.getBooking(i).getTotalPrice() + " CZK");
         }
     }
 
-//print guest statistics
-    private void printGuestStatistics() {
+    private static void printGuestStatistics(BookingManager bookingList) {
         System.out.println("\n-------Guest Statistics------------------------------------------------------------------");
 
         int bookingsWithOneGuest = 0;
@@ -100,25 +64,60 @@ public class Main {
         System.out.println("Bookings with > 2 guests: " + bookingsWithMoreGuests);
     }
 
-//print total price per booking
-    private void printTotalPricePerBooking() {
-        System.out.println("\n-------Total price per booking-----------------------------------------------------------");
+    private static void printFirstEightVacationBookings(BookingManager bookingList) {
+        System.out.println("\n-------First eight vacation bookings------------------------------------------------------");
+        int firstEight = 0;
         for (int i = 0; i < bookingList.getNumberOfBookings(); i++) {
-            System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
-                    + ": " + bookingList.getBooking(i).getGuest()+ ", "+ bookingList.getBooking(i).getBookingLength()
-                    + " nights, Total price of booking: " + bookingList.getBooking(i).getTotalPrice() + " CZK");
+            if (bookingList.getBooking(i).isVacation()) {
+                String vacationOrBusiness = bookingList.getBooking(i).isVacation() ? "vacation" : "business";
+                System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
+                        + ": " + bookingList.getBooking(i).getGuest() + ", [total guests: " + bookingList.getBooking(i).getNumberOfGuests() + ", "
+                        + "room with seaview" + "] for " + bookingList.getBooking(i).getPricePerNight() +
+                        " CZK, type of stay: " + vacationOrBusiness);
+                firstEight++;
+                if (firstEight == 8) break;
+            }
         }
     }
 
-    public static void main(String[] args) {
-        Main main = new Main();
-        main.createGuests();
-        main.createRooms();
-        main.createBookings();
-        main.printAllBookings();
-        main.printFirstEightVacationBookings();
-        main.printGuestStatistics();
-        main.printTotalPricePerBooking();
+    private static void printAllBookingsWithDetails(BookingManager bookingList) {
+        System.out.println("\n-------all bookings----------------------------------------------------------------------");
+        for (int i = 0; i < bookingList.getNumberOfBookings(); i++) {
+            String seaViewAvailability = bookingList.getBooking(i).isHasSeaView() ? "room with seaview" : "room without seaview";
+            System.out.println(bookingList.getBooking(i).getFormattedDateFrom() + " to " + bookingList.getBooking(i).getFormattedDateTo()
+                    + ": " + bookingList.getBooking(i).getGuest() + ", [total guests: " + bookingList.getBooking(i).getNumberOfGuests() + ", "
+                    + seaViewAvailability + "] for " + bookingList.getBooking(i).getPricePerNight() + " CZK");
+        }
+    }
+
+    private static void addBookings(BookingManager bookingList, List<Room> roomsList, List<Guest> guestsList) {
+        bookingList.addBooking(new Booking(roomsList.get(2), guestsList.get(0), LocalDate.of(2023, 6, 1),
+                LocalDate.of(2023, 6, 7), false));
+
+        bookingList.addBooking(new Booking(roomsList.get(1), guestsList.get(1), LocalDate.of(2023, 7, 18),
+                LocalDate.of(2023, 7, 21), true));
+
+        //adding multiple bookings with same parameters, but consequent dates
+        LocalDate startDate = LocalDate.of(2023, 8, 1);
+        for (int i = 0; i < 10; i++) {
+            LocalDate endDate = startDate.plusDays(1);
+            bookingList.addBooking(new Booking(roomsList.get(1), guestsList.get(2), startDate, endDate, true));
+            startDate = endDate.plusDays(1);
+        }
+        bookingList.addBooking(new Booking(roomsList.get(2), guestsList.get(2), LocalDate.of(2023, 8, 1),
+                LocalDate.of(2023, 8, 31), true));
+    }
+
+    private static void createRooms(List<Room> roomsList) {
+        roomsList.add(new Room(1, 1, true, true, BigDecimal.valueOf(1000)));
+        roomsList.add(new Room(2, 1, true, true, BigDecimal.valueOf(1000)));
+        roomsList.add(new Room(3, 3, false, true, BigDecimal.valueOf(2400)));
+    }
+
+    private static void createGuests(List<Guest> guestsList) {
+        guestsList.add(new Guest("Karel", "Dvořák", LocalDate.of(1990, 5, 15)));
+        guestsList.add(new Guest("Karel", "Dvořák", LocalDate.of(1979, 1, 3)));
+        guestsList.add(new Guest("Karolína", "Tmavá", LocalDate.of(1981, 6, 3)));
     }
 
 }
